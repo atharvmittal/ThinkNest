4a36d15f85a432b5722a7b09b62fe5df5d89dd59
atharvmittal <atharv.22b0101030@abes.ac.in>
2025-08-06 17:13:13 +0530

    initial Commit

---
+++ backend/src/controllers/notesController.js
@@ -0,0 +1,63 @@
+import Note from '../models/note.js';
+export async function getAllNotes(req, res) {
+  try{
+    const notes=await Note.find().sort({createdAt: -1});
+    res.status(200).json(notes);
+
+  }catch(error){
+    console.log(`error in getAllNotes controller: ${error}`);
+    res.status(500).json({message:"Internal ServerError"})
+  }
+}; 
+export async function getNoteById(req, res) {
+  try {
+    const note = await Note.findById(req.params.id);
+    if (!note) return res.status(404).json({ message: "Note not found!" });
+    res.json(note);
+  } catch (error) {
+    console.error("Error in getNoteById controller", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}; 
+
+export async function createNote(req, res) {
+    try {
+      const {title ,content}=req.body;
+      const newNote=new Note({title,content});
+      const savedNote=await newNote.save();
+      res.status(201).json(savedNote)
+
+      
+    } catch (error) {
+        console.log(`error in createNote controller: ${error}`);
+        res.status(500).json({message:"Internal ServerError"});
+      
+    }
+}; 
+
+export async function updateNote(req, res) {
+  try {
+    const {title, content} = req.body;
+    const updatedNote=await Note.findByIdAndUpdate(req.params.id,{title, content}, {new: true});
+    if (!updatedNote) {
+      return res.status(404).json({message:"Note not found"});
+    }
+    res.status(200).json({message:"Note updated successfully"});
+    
+  } catch (error) {
+    console.log(`error in updateNote controller: ${error}`);
+    res.status(500).json({message:"Internal ServerError"});
+    
+  }
+}; 
+
+export async function deleteNote(req, res) {
+  try {
+    const deletedNote = await Note.findByIdAndDelete(req.params.id);
+    if (!deletedNote) return res.status(404).json({ message: "Note not found" });
+    res.status(200).json(deletedNote);
+  } catch (error) {
+    console.error("Error in deleteNote controller", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}; 
---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import HomePage from './pages/homePage'
+import NoteDetail from './pages/noteDetail'
+import CreatePage from './pages/CreatePage'
+import { Routes, Route } from 'react-router'
+const App = () => {
+  return (
+    <div className="relative h-full w-full">
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/note/:id" element={<NoteDetail />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
